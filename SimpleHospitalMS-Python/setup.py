965c59455009ae17021a41a5e66afb95cd152d04
vignesh7311 <vigneshnaroju@gmail.com>
2023-12-30 20:53:07 +0530

    first commit

---
+++ SimpleHospitalMS-Python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Hospital Management',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='bhuvan',
+    author_email='bhuvansaisagala@gmail.com',
+    description='Hospital Managment'
+)
